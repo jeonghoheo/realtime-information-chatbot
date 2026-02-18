ad05016ba4dcbf826c21ede591b83fffe91cc2c4
jeonghoheo <heojeongho1992@gmail.com>
2026-02-18 06:07:44 +0300

    feat: install packages

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from realtime-info-chatbot!")
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+import requests
+from bs4 import BeautifulSoup
 
+llm = ChatOllama(model="qwen2.5-coder")
 
-if __name__ == "__main__":
-    main()
